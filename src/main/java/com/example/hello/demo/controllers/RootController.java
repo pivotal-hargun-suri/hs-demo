275bff41d14106cd0520d67032cf0e37407ab049
pivotal-hargun-suri <hsuri@pivotal.io>
2019-03-14 14:05:55 -0400

    Added profile.

--- src/main/java/com/example/hello/demo/controllers/RootController.java
+++ src/main/java/com/example/hello/demo/controllers/RootController.java
@@ -2,15 +2,20 @@ package com.example.hello.demo.controllers;
 
 import com.example.hello.demo.models.Something;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 public class RootController {
     private Something something = new Something();
+    @Value("${current-profile}") String profile;
 
     @RequestMapping("/")
     public Something Something() {
+        something.setMessage("Profile set: " + profile);
         return something;
     }
 }
